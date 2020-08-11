7c45232b9c804b7e6784c76f367ef35a61716c1b
H. Michael Ko <hyosunko@gmail.com>
2020-08-11 13:34:20 -0700

    Add Dropdown Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Accordion from './components/Accordion'
 import Search from './components/Search'
+import Dropdown from './components/Dropdown'
 import "./App.css";
 
 const items = [
@@ -19,10 +20,31 @@ const items = [
   },
 ]
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  },
+]
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0])
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   )
 }
