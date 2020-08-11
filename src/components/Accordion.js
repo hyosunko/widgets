1b92f86d2d2dfc86777f2c9966aad24889ee37c0
H. Michael Ko <hyosunko@gmail.com>
2020-08-11 09:50:25 -0700

    Add Accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react'
+
+const Accordion = ({ items }) => {
+  const renderedItems = items.map(item => {
+    return(
+      <Fragment key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </Fragment>
+    )
+  })
+
+  return (
+  <div>{renderedItems}</div>
+  )
+}
+
+export default Accordion
