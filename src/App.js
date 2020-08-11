e12c6a49913cb0d080935a7cfad7ccb5cd157a32
H. Michael Ko <hyosunko@gmail.com>
2020-08-11 11:20:53 -0700

    Add Search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import Accordion from './components/Accordion'
+import Search from './components/Search'
+import "./App.css";
 
 const items = [
   {
@@ -20,7 +22,7 @@ const items = [
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   )
 }
