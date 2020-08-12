a2f9bcf82fed6f202ce00ca733b3f2ebcf60f91c
H. Michael Ko <hyosunko@gmail.com>
2020-08-12 14:08:34 -0700

    create custom hooks to extract video search

--- src/components/videos/Video.js
+++ src/components/videos/Video.js
@@ -3,32 +3,21 @@ import React, { useEffect, useState } from 'react'
 import SearchBar from './SearchBar'
 import VideoList from './VideoList'
 import VideoDetail from './VideoDetail'
-import youtube from '../../apis/youtube'
+import useVideos from '../../hooks/useVideos'
 
 const Video = () => {
-  const [videos, setVideos] = useState([])
+  const [videos, search] = useVideos("black pink");
   const [selectedVideo, setSelectedVideo] = useState(null)
 
   useEffect(() => {
-    onSearchTermSubmit('black pink')
-
-  },[])
-
-  const onSearchTermSubmit = async searchTerm => {
-    const res = await youtube.get("/search", {
-      params: {
-        q: searchTerm,
-      },
-    });
-    setVideos(res.data.items);
-    setSelectedVideo(res.data.items[0]);
-  }
+    setSelectedVideo(videos[0])
+  }, [videos])
 
   const onVideoSelect = video => setSelectedVideo(video)
 
   return (
     <div className="ui container">
-      <SearchBar onSearchTermSubmit={onSearchTermSubmit} />
+      <SearchBar onSearchTermSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
