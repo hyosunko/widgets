20d6cb68d9c6d79c4ce590910aa12bdd8ae9fb9a
H. Michael Ko <hyosunko@gmail.com>
2020-08-11 17:52:41 -0700

    Add Header component for menu list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import Accordion from './components/Accordion'
 import Dropdown from './components/Dropdown'
+import Header from './components/Header'
 import Route from './components/Route'
 import Search from './components/Search'
 import Translate from './components/Translate'
@@ -42,6 +43,7 @@ const App = () => {
 
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
